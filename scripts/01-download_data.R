2dd21160a93dc47c81ca4fe9bdb8ea9ae5f30d52
Mohid <mohid.sharif@mail.utoronto.ca>
2024-03-06 02:07:24 +0000

    Downloaded and cleaned data. Created 3 separate tables: Gender deaths over the years, age group deaths over the years, and the table containing both.

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,34 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from opendatatoronto
+# Author 1: Mohid Sharif
+# Author 2: Gavin Crooks
+# Author 3: Samarth
+# Date: 12 Marth 2024
+# Contact: mohid.sharif@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Any other information needed? 
 
 
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+package <- show_package("a7ae08f3-c512-4a88-bb3c-ab40eca50c5e")
+package
 
+# Obtain the package resource
+resources <- list_package_resources("a7ae08f3-c512-4a88-bb3c-ab40eca50c5e")
+
+# Filter the csv files
+datastore_resources <- filter(resources, tolower(format) %in% c('csv'))
+
+# Extract the csv file of interest
+homeless_deaths <- filter(datastore_resources, row_number()==3) %>% get_resource()
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(homeless_deaths, "~/Drug-Toxicity-and-Homelessness/data/raw_data/raw_data.csv")
 
-         
+
--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -11,34 +11,429 @@
 library(tidyverse)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+raw_data <- read_csv("~/Drug-Toxicity-and-Homelessness/data/raw_data/raw_data.csv")
 
+
+# Rename year of death column
+names(raw_data)[names(raw_data) == 'Year of death'] <- 'Year_of_death'
+
+# Clean data to remove unkown values and isolate causes of deaths relating to drug toxicity
 cleaned_data <-
   raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+  filter(Cause_of_death == "Drug Toxicity", Age_group != "Unknown", Gender != "Unknown") |>
+  # Pipe this to include only year of death, age group, gender, and count.
+  select(Year_of_death, Age_group, Gender, Count)
+
+
+head(cleaned_data, n=10)
+
+
+#### Gender data ####
+
+### Obtain Male deaths for each year ###
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2017, Gender == 'Male') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+male_2017_deaths <- sum(new_data$Count)
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2018, Gender == 'Male') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+male_2018_deaths <- sum(new_data$Count)
+  
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2019, Gender == 'Male') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+male_2019_deaths <- sum(new_data$Count)
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2020, Gender == 'Male') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+male_2020_deaths <- sum(new_data$Count)
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2021, Gender == 'Male') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+male_2021_deaths <- sum(new_data$Count)
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2022, Gender == 'Male') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+male_2022_deaths <- sum(new_data$Count)
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2023, Gender == 'Male') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+male_2023_deaths <- sum(new_data$Count)
+
+### Obtain Female deaths for each year ###
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2017, Gender == 'Female') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+female_2017_deaths <- sum(new_data$Count)
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2018, Gender == 'Female') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+female_2018_deaths <- sum(new_data$Count)
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2019, Gender == 'Female') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+female_2019_deaths <- sum(new_data$Count)
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2020, Gender == 'Female') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+female_2020_deaths <- sum(new_data$Count)
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2021, Gender == 'Female') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+female_2021_deaths <- sum(new_data$Count)
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2022, Gender == 'Female') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+female_2022_deaths <- sum(new_data$Count)
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2023, Gender == 'Female') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+female_2023_deaths <- sum(new_data$Count)
+
+### Create the data ###
+
+gender_deaths_data <-
+  tibble(
+    Year = c('2017', '2017', '2018', '2018', '2019', '2019', '2020', '2020', '2021', '2021', '2022', '2022', '2023', '2023'),
+    Gender = c('Male', 'Female', 'Male', 'Female','Male', 'Female','Male', 'Female','Male', 'Female','Male', 'Female','Male', 'Female'),
+    Deaths = c(male_2017_deaths, female_2017_deaths, male_2018_deaths, female_2018_deaths, male_2019_deaths, female_2019_deaths, male_2020_deaths, female_2020_deaths, male_2021_deaths, female_2021_deaths, male_2022_deaths, female_2022_deaths, male_2023_deaths, female_2023_deaths)
+  )
+
+#### Age Group Data ####
+
+### Obtain all age group data for 2017 ###
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2017, Age_group == '<20') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+youth_2017_deaths <- sum(new_data$Count)
+
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2017, Age_group == '20-39') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+adults_2017_deaths <- sum(new_data$Count)
+
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2017, Age_group == '40-59') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+adults2_2017_deaths <- sum(new_data$Count)
+
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2017, Age_group == '60+') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+elderly_2017_deaths <- sum(new_data$Count)
+
+
+
+### Obtain all age group data for 2018 ###
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2018, Age_group == '<20') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+youth_2018_deaths <- sum(new_data$Count)
+
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2018, Age_group == '20-39') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+adults_2018_deaths <- sum(new_data$Count)
+
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2018, Age_group == '40-59') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+adults2_2018_deaths <- sum(new_data$Count)
+
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2018, Age_group == '60+') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+elderly_2018_deaths <- sum(new_data$Count)
+
+
+### Obtain all age group data for 2019 ###
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2019, Age_group == '<20') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+youth_2019_deaths <- sum(new_data$Count)
+
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2019, Age_group == '20-39') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+adults_2019_deaths <- sum(new_data$Count)
+
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2019, Age_group == '40-59') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+adults2_2019_deaths <- sum(new_data$Count)
+
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2019, Age_group == '60+') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+elderly_2019_deaths <- sum(new_data$Count)
+
+
+### Obtain all age group data for 2020 ###
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2020, Age_group == '<20') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+youth_2020_deaths <- sum(new_data$Count)
+
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2020, Age_group == '20-39') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+adults_2020_deaths <- sum(new_data$Count)
+
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2020, Age_group == '40-59') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+adults2_2020_deaths <- sum(new_data$Count)
+
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2020, Age_group == '60+') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+elderly_2020_deaths <- sum(new_data$Count)
+
+
+### Obtain all age group data for 2021 ###
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2021, Age_group == '<20') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+youth_2021_deaths <- sum(new_data$Count)
+
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2021, Age_group == '20-39') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+adults_2021_deaths <- sum(new_data$Count)
+
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2021, Age_group == '40-59') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+adults2_2021_deaths <- sum(new_data$Count)
+
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2021, Age_group == '60+') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+elderly_2021_deaths <- sum(new_data$Count)
+
+
+### Obtain all age group data for 2022 ###
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2022, Age_group == '<20') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+youth_2022_deaths <- sum(new_data$Count)
+
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2022, Age_group == '20-39') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+adults_2022_deaths <- sum(new_data$Count)
+
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2022, Age_group == '40-59') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+adults2_2022_deaths <- sum(new_data$Count)
+
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2022, Age_group == '60+') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+elderly_2022_deaths <- sum(new_data$Count)
+
+
+### Obtain all age group data for 2023 ###
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2023, Age_group == '<20') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+youth_2023_deaths <- sum(new_data$Count)
+
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2023, Age_group == '20-39') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+adults_2023_deaths <- sum(new_data$Count)
+
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2023, Age_group == '40-59') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+adults2_2023_deaths <- sum(new_data$Count)
+
+
+new_data <-
+  cleaned_data |>
+  filter(Year_of_death == 2023, Age_group == '60+') |>
+  select(Year_of_death, Gender, Count)
+head(new_data, n=10)
+
+elderly_2023_deaths <- sum(new_data$Count)
+
+
+#### Create the data ###
+
+age_group_deaths_data <-
+  tibble(
+    Year = c('2017', '2017', '2017', '2017', '2018', '2018', '2018', '2018', '2019', '2019', '2019', '2019', '2020', '2020', '2020', '2020', '2021', '2021', '2021', '2021', '2022', '2022', '2022', '2022', '2023', '2023', '2023', '2023'),
+    Age_group = c('<20', '20-39', '40-59', '60+', '<20', '20-39', '40-59', '60+', '<20', '20-39', '40-59', '60+', '<20', '20-39', '40-59', '60+', '<20', '20-39', '40-59', '60+', '<20', '20-39', '40-59', '60+', '<20', '20-39', '40-59', '60+'),
+    Deaths = c(youth_2017_deaths, adults_2017_deaths, adults2_2017_deaths, elderly_2017_deaths, youth_2018_deaths, adults_2018_deaths, adults2_2018_deaths, elderly_2018_deaths, youth_2019_deaths, adults_2019_deaths, adults2_2019_deaths, elderly_2019_deaths, youth_2020_deaths, adults_2020_deaths, adults2_2020_deaths, elderly_2020_deaths, youth_2021_deaths, adults_2021_deaths, adults2_2021_deaths, elderly_2021_deaths, youth_2022_deaths, adults_2022_deaths, adults2_2022_deaths, elderly_2022_deaths, youth_2023_deaths, adults_2023_deaths, adults2_2023_deaths, elderly_2023_deaths)
+  )
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(cleaned_data, "data/cleaned_data.csv")
+write_csv(gender_deaths_data, "data/gender_deaths_data.csv")
+write_csv(age_group_deaths_data, "data/age_group_deaths_data.csv")
